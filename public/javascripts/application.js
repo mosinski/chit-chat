3b4af624b5c4b168e9a2161533e84219f03bdecf
Miłosz Osiński <mylosz@yahoo.pl>
2014-04-27 22:51:07 +0200

    add auto scroll down, and use heroku faye server

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('http://localhost:9292/faye');
+  var faye = new Faye.Client('http://lightchat-server.herokuapp.com/faye');
   faye.subscribe("/messages/new", function(data) {
     eval(data);
   });
