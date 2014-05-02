df5efa08b2e50c54282a499db6bbfee83698e38e
Miłosz Osiński <mylosz@yahoo.pl>
2014-05-02 14:40:15 +0200

    add i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
+  def set_locale
+    session[:locale] = params[:locale] if params[:locale]
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
