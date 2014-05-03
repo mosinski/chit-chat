3d92ad00a0dd161d51e7f7fca30b76410e63afbc
Miłosz Osiński <mylosz@yahoo.pl>
2014-05-03 23:36:58 +0200

    add all fonts localy

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module Chatter
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
     config.filter_parameters += [:password]
-    config.time_zone = "Warsaw"
+    config.time_zone = 'Warsaw'
   end
 end
