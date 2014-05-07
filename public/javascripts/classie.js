55f83799533fc53817a92a0ef11b0d0f86526084
Miłosz Osiński <mylosz@yahoo.pl>
2014-05-07 18:54:56 +0200

    correct javascripts

--- public/javascripts/classie.js
+++ public/javascripts/classie.js
@@ -1,80 +1,56 @@
-/*!
- * classie - class helper functions
- * from bonzo https://github.com/ded/bonzo
- * 
- * classie.has( elem, 'my-class' ) -> true/false
- * classie.add( elem, 'my-new-class' )
- * classie.remove( elem, 'my-unwanted-class' )
- * classie.toggle( elem, 'my-class' )
- */
+( function(window) {
+  'use strict';
 
-/*jshint browser: true, strict: true, undef: true */
-/*global define: false */
+  function classReg(className) {
+    return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+  }
 
-( function( window ) {
+  var hasClass, addClass, removeClass;
 
-'use strict';
+  if ( 'classList' in document.documentElement ) {
+    hasClass = function(elem, c) {
+      return elem.classList.contains(c);
+    };
+    addClass = function(elem, c) {
+      elem.classList.add(c);
+    };
+    removeClass = function(elem, c) {
+      elem.classList.remove(c);
+    };
+  }
+  else {
+    hasClass = function(elem, c) {
+      return classReg(c).test(elem.className);
+    };
+    addClass = function(elem, c) {
+      if (!hasClass(elem, c)) {
+        elem.className = elem.className + ' ' + c;
+      }
+    };
+    removeClass = function(elem, c) {
+      elem.className = elem.className.replace(classReg(c), ' ');
+    };
+  }
 
-// class helper functions from bonzo https://github.com/ded/bonzo
+  function toggleClass(elem, c) {
+    var fn = hasClass(elem, c) ? removeClass : addClass;
+    fn(elem, c);
+  }
 
-function classReg( className ) {
-  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-}
-
-// classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
-var hasClass, addClass, removeClass;
-
-if ( 'classList' in document.documentElement ) {
-  hasClass = function( elem, c ) {
-    return elem.classList.contains( c );
+  var classie = {
+    hasClass: hasClass,
+    addClass: addClass,
+    removeClass: removeClass,
+    toggleClass: toggleClass,
+    has: hasClass,
+    add: addClass,
+    remove: removeClass,
+    toggle: toggleClass
   };
-  addClass = function( elem, c ) {
-    elem.classList.add( c );
-  };
-  removeClass = function( elem, c ) {
-    elem.classList.remove( c );
-  };
-}
-else {
-  hasClass = function( elem, c ) {
-    return classReg( c ).test( elem.className );
-  };
-  addClass = function( elem, c ) {
-    if ( !hasClass( elem, c ) ) {
-      elem.className = elem.className + ' ' + c;
-    }
-  };
-  removeClass = function( elem, c ) {
-    elem.className = elem.className.replace( classReg( c ), ' ' );
-  };
-}
 
-function toggleClass( elem, c ) {
-  var fn = hasClass( elem, c ) ? removeClass : addClass;
-  fn( elem, c );
-}
-
-var classie = {
-  // full names
-  hasClass: hasClass,
-  addClass: addClass,
-  removeClass: removeClass,
-  toggleClass: toggleClass,
-  // short names
-  has: hasClass,
-  add: addClass,
-  remove: removeClass,
-  toggle: toggleClass
-};
-
-// transport
-if ( typeof define === 'function' && define.amd ) {
-  // AMD
-  define( classie );
-} else {
-  // browser global
-  window.classie = classie;
-}
-
-})( window );
+  if (typeof define === 'function' && define.amd) {
+    define(classie);
+  } else {
+    window.classie = classie;
+  }
+})(window);
--- public/javascripts/mediaCheck.js
+++ public/javascripts/mediaCheck.js
@@ -1,100 +1,89 @@
-var mediaCheck = function( options ) {
+var mediaCheck = function(options) {
   var mq, mqChange, createListener, convertEmToPx, getPXValue,
       matchMedia = window.matchMedia !== undefined;
 
-  if ( matchMedia ) {
-    mqChange = function( mq, options ) {
-      if ( mq.matches ) {
-        if ( typeof options.entry === "function" ) {
+  if (matchMedia) {
+    mqChange = function(mq, options) {
+      if (mq.matches) {
+        if (typeof options.entry === "function") {
           options.entry();
         }
-      } else if ( typeof options.exit === "function" ) {
+      } else if (typeof options.exit === "function") {
         options.exit();
       }
-      if ( typeof options.both === "function" ) {
+      if (typeof options.both === "function") {
         options.both();
       }
     };
-    // Has matchMedia support
     createListener = function() {
-      mq = window.matchMedia( options.media );
-      mq.addListener( function() {
-        mqChange( mq, options );
+      mq = window.matchMedia(options.media);
+      mq.addListener(function() {
+        mqChange(mq, options);
       });
-      window.addEventListener( "orientationchange", function() {
-        mq = window.matchMedia( options.media );
-        mqChange( mq, options );
+      window.addEventListener("orientationchange", function() {
+        mq = window.matchMedia(options.media);
+        mqChange(mq, options);
       }, false );
-      mqChange( mq, options );
+      mqChange(mq, options);
     };
     createListener();
 
   } else {
-    // pageWidth is initialized during initial match
     var pageWidth,
         breakpoints = {};
 
-    mqChange = function( mq, options ) {
-      if ( mq.matches ) {
-        if ( typeof options.entry === "function" && ( breakpoints[options.media] === false || breakpoints[options.media] == null )) {
+    mqChange = function(mq, options) {
+      if (mq.matches) {
+        if (typeof options.entry === "function" && (breakpoints[options.media] === false || breakpoints[options.media] == null)) {
           options.entry();
         }
-      } else if ( typeof options.exit === "function" && ( breakpoints[options.media] === true || breakpoints[options.media] == null )) {
+      } else if (typeof options.exit === "function" && (breakpoints[options.media] === true || breakpoints[options.media] == null)) {
         options.exit();
       }
-      if ( typeof options.both === "function" ) {
+      if (typeof options.both === "function") {
         options.both();
       }
-
       breakpoints[options.media] = mq.matches;
     };
 
-    convertEmToPx = function( value ) {
+    convertEmToPx = function(value) {
       var emElement;
 
-      emElement = document.createElement( "div" );
+      emElement = document.createElement("div");
       emElement.style.width = "1em";
-      document.body.appendChild( emElement );
+      document.body.appendChild(emElement);
 
       return value * emElement.offsetWidth;
     };
 
-    getPXValue = function( width, unit ) {
+    getPXValue = function(width, unit) {
       var value;
 
-      switch ( unit ) {
+      switch (unit) {
       case "em":
-        value = convertEmToPx( width );
+        value = convertEmToPx(width);
         break;
       default:
         value = width;
       }
-
       return value;
     };
 
-    // Create list of breakpoints
-    for ( i in options ) {
+    for (i in options) {
       breakpoints[options.media] = null;
     }
 
-    // No matchMedia support
     var mmListener = function() {
       var parts = options.media.match( /\((.*)-.*:\s*([\d\.]*)(.*)\)/ ),
           constraint = parts[ 1 ],
-          value = getPXValue( parseInt( parts[ 2 ], 10 ), parts[3] ),
+          value = getPXValue(parseInt( parts[ 2 ], 10), parts[3] ),
           fakeMatchMedia = {},
           clientWidth = document.documentElement.clientWidth;
 
-
-      // scope this to width changes to prevent small-screen scrolling (browser chrome off-screen)
-      //   from triggering a change
       if (pageWidth != clientWidth) {
         fakeMatchMedia.matches = constraint === "max" && value > clientWidth ||
                                  constraint === "min" && value < clientWidth;
-        mqChange( fakeMatchMedia, options );
-
-        // reset pageWidth
+        mqChange(fakeMatchMedia, options);
         pageWidth = clientWidth;
       }
     };
